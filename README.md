11eb4c696231c62d230124bee13bdb688f49407b
Kien Le <kienle371999@gmail.com>
2025-04-25 07:28:21 +0700

    Add error message and required documents

--- README.md
+++ README.md
@@ -4,18 +4,18 @@ The last phase aims to integrate Ethereum testnet and machine learning models. T
 
 ## Install forge
 
-```bash
+```
 curl -L https://foundry.paradigm.xyz | bash
 ```
 
 ## Deployment
 
-```bash
+```
 forge create --rpc-url <your_rpc_url> --private-key <your_private_key> src/MLModels.sol:MLModels
 ```
 
 ## Build server
 
-```bash
+```
 flask --app app.py run --host=0.0.0.0 --port=8000
 ```
--- contract_connector/app.py
+++ contract_connector/app.py
@@ -5,18 +5,21 @@ app = Flask(__name__)
 
 @app.post('/push-model')
 def push_model():
-    data = request.json
-    timestamp = int(data['timestamp'])
-    threat_level = int(data['threatLevel'])
-    ip_address = data['ipAddress']
+    try:
+        data = request.json
+        timestamp = int(data['timestamp'])
+        threat_level = int(data['threatLevel'])
+        ip_address = data['ipAddress']
 
-    service = Service()
-    tx_hash = service.push_model(timestamp, threat_level, ip_address)
+        service = Service()
+        tx_hash = service.push_model(timestamp, threat_level, ip_address)
 
-    return jsonify({
-        'message': 'Model is pushed successfully',
-        'txHash': tx_hash
-    }), 200
+        return jsonify({
+            'message': 'Model is pushed successfully',
+            'txHash': tx_hash
+        }), 200
+    except Exception as e:
+        return jsonify({ "error": str(e) }), 400
 
 @app.get('/get-models')
 def get_models():
